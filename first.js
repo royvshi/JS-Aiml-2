5fb442df50c19c27db9d4155d955060872a634cd
Royal Vshishtha <royalvshishtha123@gmail.com>
2026-05-30 21:28:21 +0530

    add experiment-1 html files

---
+++ first.js
@@ -0,0 +1,2 @@
+name= ("royal")
+console.log(name)
---
+++ node-proj/app.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    {/* <audio controls>
+      <source src="" type="audio/mp3/"></source>
+    </audio> */}
+    {/* <video controls>
+      <source src="" type="video/mp4/"></source>
+    </video> */}
+    <iframe src="https://youtu.be/9mH-57TvCGo?si=Q3hSzfQIidTeS5W9"></iframe> 
+    <img src="https://i.postimg.cc/26pRWsnM/bgmi-profile.jpg" width="250px" height={250}></img>
+    <button>Click ME</button>
+    <h1>welcome to my webpages</h1>
+    <h2>Hello</h2>
+    <p>AIML-2</p>
+    </>
+    
+  )n
+}
+
+export default App-
